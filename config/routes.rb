a170c8d55ce7cf5150e68b071ca8251a0b7d9358
Gibral Raphael <graphael@Norths-MacBook-Pro.local>
2017-12-19 13:45:28 -0500

    creating profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :admins, path: 'admins', controllers: { sessions: "admins/sessions"}
+  # eg. http://localhost:3000/admins/sign_in
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions"}
+  # eg. http://localhost:3000/users/sign_in
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
